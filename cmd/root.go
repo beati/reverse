4425b2b7b9141839fe563856e725beeaf53d7384
Thomas Beati <thomas.beati@gmail.com>
2017-06-06 16:16:30 +0200

    Make config file mandatory

--- cmd/root.go
+++ cmd/root.go
@@ -64,7 +64,9 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
 	}
 }
